a537bf22412890ae47abb1c6af162cc22d525a64
T4rp <why9ten@gmail.com>
2025-12-07 14:08:09 -0500

    feat: add vulkan contex

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,137 @@
+use std::borrow::Cow;
+use std::ffi;
 use std::rc::Rc;
 
+use ash::Entry;
+use ash::vk::{
+    self, ApplicationInfo, DebugUtilsMessageSeverityFlagsEXT, DebugUtilsMessageTypeFlagsEXT,
+    DebugUtilsMessengerCallbackDataEXT, DebugUtilsMessengerCreateInfoEXT,
+};
+use winit::raw_window_handle::{HasDisplayHandle, HasWindowHandle};
 use winit::{
     application::ApplicationHandler,
-    event::{DeviceId, KeyEvent, WindowEvent},
+    event::WindowEvent,
     event_loop::EventLoop,
     keyboard::{KeyCode, PhysicalKey},
     window::{Window, WindowAttributes},
 };
 
+const USE_VALIDATION_LAYERS: bool = true;
+
 struct App {
     window: Option<Rc<Window>>,
+    vk_ctx: Option<VulkanContext>,
 }
 
 impl App {
     fn new() -> Self {
-        Self { window: None }
+        Self {
+            window: None,
+            vk_ctx: None,
+        }
+    }
+}
+
+unsafe extern "system" fn debug_messager_callback(
+    message_severity: DebugUtilsMessageSeverityFlagsEXT,
+    message_type: DebugUtilsMessageTypeFlagsEXT,
+    callback_data: *const DebugUtilsMessengerCallbackDataEXT,
+    _userdata: *mut std::os::raw::c_void,
+) -> vk::Bool32 {
+    unsafe {
+        let callback_data = *callback_data;
+        let message_id_number = callback_data.message_id_number;
+
+        let message_id_name = if callback_data.p_message_id_name.is_null() {
+            Cow::from("")
+        } else {
+            ffi::CStr::from_ptr(callback_data.p_message_id_name).to_string_lossy()
+        };
+
+        let message = if callback_data.p_message.is_null() {
+            Cow::from("")
+        } else {
+            ffi::CStr::from_ptr(callback_data.p_message).to_string_lossy()
+        };
+
+        println!(
+            "{message_severity:?}:\n{message_type:?} [{message_id_name} ({message_id_number})] : {message}\n",
+        );
+
+        vk::FALSE
+    }
+}
+
+struct VulkanContext {
+    entry: Entry,
+    instance: ash::Instance,
+    surface: vk::SurfaceKHR,
+}
+
+impl VulkanContext {
+    fn new(window: Rc<Window>) -> Self {
+        let raw_window_handle = window.window_handle().unwrap().as_raw();
+        let raw_display_handle = window.display_handle().unwrap().as_raw();
+
+        unsafe {
+            let entry = Entry::load().unwrap();
+
+            let mut extensions = vec![ash::ext::debug_utils::NAME.as_ptr()];
+            let mut validation_layers = vec![];
+
+            if USE_VALIDATION_LAYERS {
+                validation_layers.push(c"VK_LAYER_KHRONOS_validation".as_ptr())
+            }
+
+            let surface_extensions =
+                ash_window::enumerate_required_extensions(raw_display_handle).unwrap();
+
+            extensions.extend_from_slice(surface_extensions);
+
+            let appinfo = ApplicationInfo::default()
+                .application_name(c"HexxEngine")
+                .api_version(ash::vk::API_VERSION_1_3);
+
+            let create_info = vk::InstanceCreateInfo::default()
+                .application_info(&appinfo)
+                .enabled_extension_names(&extensions)
+                .enabled_layer_names(&validation_layers);
+
+            let instance = entry.create_instance(&create_info, None).unwrap();
+            let debug_utils_fn = ash::ext::debug_utils::Instance::new(&entry, &instance);
+
+            let messager_create_info = DebugUtilsMessengerCreateInfoEXT::default()
+                .message_severity(
+                    vk::DebugUtilsMessageSeverityFlagsEXT::ERROR
+                        | vk::DebugUtilsMessageSeverityFlagsEXT::WARNING
+                        | vk::DebugUtilsMessageSeverityFlagsEXT::INFO,
+                )
+                .message_type(
+                    vk::DebugUtilsMessageTypeFlagsEXT::GENERAL
+                        | vk::DebugUtilsMessageTypeFlagsEXT::VALIDATION
+                        | vk::DebugUtilsMessageTypeFlagsEXT::PERFORMANCE,
+                )
+                .pfn_user_callback(Some(debug_messager_callback));
+
+            debug_utils_fn
+                .create_debug_utils_messenger(&messager_create_info, None)
+                .unwrap();
+
+            let surface = ash_window::create_surface(
+                &entry,
+                &instance,
+                raw_display_handle,
+                raw_window_handle,
+                None,
+            )
+            .unwrap();
+
+            Self {
+                entry,
+                instance,
+                surface,
+            }
+        }
     }
 }
 
@@ -26,7 +143,10 @@ impl ApplicationHandler for App {
                 .unwrap(),
         );
 
+        let vk_ctx = VulkanContext::new(window.clone());
+
         self.window = Some(window);
+        self.vk_ctx = Some(vk_ctx);
     }
 
     fn window_event(
