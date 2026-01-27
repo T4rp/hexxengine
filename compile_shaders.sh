00915594a96c162c8142c130f4993126ca7a11f9
T4rp <why9ten@gmail.com>
2026-01-27 10:19:14 -0500

    chore: build shader modules outside of source directory

--- compile_shaders.sh
+++ compile_shaders.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-glslc shaders/main.vert -o shaders/base.vert.spv
-glslc shaders/main.frag -o shaders/base.frag.spv
+glslc shaders/main.vert -o assets/base.vert.spv
+glslc shaders/main.frag -o assets/base.frag.spv
 
-glslc shaders/shadow.vert -o shaders/shadow.vert.spv
-glslc shaders/shadow.frag -o shaders/shadow.frag.spv
+glslc shaders/shadow.vert -o assets/shadow.vert.spv
+glslc shaders/shadow.frag -o assets/shadow.frag.spv
 
--- src/vulkan.rs
+++ src/vulkan.rs
@@ -1991,8 +1991,8 @@ impl VulkanContext {
             .viewports(viewports)
             .scissors(scissors);
 
-        let vert_shader_code = fs::read("shaders/base.vert.spv").unwrap();
-        let frag_shader_code = fs::read("shaders/base.frag.spv").unwrap();
+        let vert_shader_code = fs::read("assets/base.vert.spv").unwrap();
+        let frag_shader_code = fs::read("assets/base.frag.spv").unwrap();
 
         let vertex_shader = create_shader_module(device, &vert_shader_code);
         let fragment_shader = create_shader_module(device, &frag_shader_code);
@@ -2110,8 +2110,8 @@ impl VulkanContext {
             .viewports(viewports)
             .scissors(scissors);
 
-        let vert_shader_code = fs::read("shaders/shadow.vert.spv").unwrap();
-        let frag_shader_code = fs::read("shaders/shadow.frag.spv").unwrap();
+        let vert_shader_code = fs::read("assets/shadow.vert.spv").unwrap();
+        let frag_shader_code = fs::read("assets/shadow.frag.spv").unwrap();
 
         let vertex_shader = create_shader_module(device, &vert_shader_code);
         let fragment_shader = create_shader_module(device, &frag_shader_code);
