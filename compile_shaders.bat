e0391b03c2ecf81d3a65cd32ba64d5b1435a0e69
T4rp <why9ten@gmail.com>
2026-03-05 22:01:06 -0500

    feat: add shader compiling script for windows

---
+++ compile_shaders.bat
@@ -0,0 +1,12 @@
+glslc shaders\main.vert -o assets\main.vert.spv
+glslc shaders\main.frag -o assets\main.frag.spv
+
+glslc shaders\shadow.vert -o assets\shadow.vert.spv
+glslc shaders\shadow.frag -o assets\shadow.frag.spv
+
+glslc shaders\skybox.vert -o assets\skybox.vert.spv
+glslc shaders\skybox.frag -o assets\skybox.frag.spv
+
+glslc shaders\main2d.vert -o assets\main2d.vert.spv
+glslc shaders\main2d.frag -o assets\main2d.frag.spv
+
