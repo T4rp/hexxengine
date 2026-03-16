c7d98f87bfc34be2bdb76edd6049ec7617d2fb66
T4rp <why9ten@gmail.com>
2026-03-16 05:30:16 -0400

    fix: correct camera fov calculation

--- engine/src/scene.rs
+++ engine/src/scene.rs
@@ -34,10 +34,7 @@ impl Camera {
     }
 
     pub fn calc_perspective_matrices(&self, aspect_ratio: f32) -> (Mat4, Mat4) {
-        let vertical_fov = 2.0
-            * (self.fov.to_radians() * 0.5)
-                .tan()
-                .atan2(1.0 / aspect_ratio);
+        let vertical_fov = 2.0 * (self.fov.to_radians() * 0.5).tan().atan2(aspect_ratio);
 
         let mut proj = Mat4::perspective_infinite_reverse_rh(vertical_fov, aspect_ratio, 1.0);
         proj.y_axis *= vec4(1.0, -1.0, 1.0, 1.0);
