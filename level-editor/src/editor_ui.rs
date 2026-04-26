14a342677729dd2f5df3190893001a067d3a4dde
T4rp <why9ten@gmail.com>
2026-04-26 19:45:20 -0400

    add method to build ui from tree

--- level-editor/src/editor_ui.rs
+++ level-editor/src/editor_ui.rs
@@ -1,4 +1,6 @@
-use hexxengine::glam::vec2;
+use hexxengine::glam::{Vec4, vec2};
+use hexxengine::renderer::renderer::WHITE_TEXTURE_INDEX;
+use hexxengine::scene::{UiDraw, UiFrame};
 use hexxengine::{
     glam::{Vec2, Vec3},
     thunderdome::{Arena, Index},
@@ -38,31 +40,82 @@ impl UiElement {
 
 pub struct UiContext {
     elements: Arena<UiNode>,
+    root: Vec<Index>,
 }
 
 impl UiContext {
     fn new() -> Self {
         Self {
             elements: Arena::new(),
+            root: Vec::new(),
         }
     }
 
-    fn new_elem(&mut self, element: UiElement, children: &[Index]) -> Index {
+    fn parent(&mut self, child: Index, parent: Index) {
+        let parent_element = self.elements.get_mut(parent).unwrap();
+        parent_element.children.push(child);
+
+        let child_element = self.elements.get_mut(child).unwrap();
+        child_element.parent = Some(parent);
+    }
+
+    fn root(&mut self, root_element: Index) {
+        self.root.push(root_element);
+    }
+
+    fn new_elem(&mut self, element: UiElement) -> Index {
         let element = UiNode {
             parent: None,
-            children: children.to_vec(),
+            children: Vec::new(),
             world_position: element.position(),
             element,
         };
 
         let index = self.elements.insert(element);
 
-        for child in children.iter() {
-            let element = self.elements.get_mut(*child).unwrap();
-            element.parent = Some(index);
+        index
+    }
+
+    fn build_draws(&mut self) -> Vec<UiDraw> {
+        let mut ui_draws = Vec::new();
+        let mut elements = self.root.clone();
+
+        while elements.len() > 0 {
+            let elem_i = elements.pop().unwrap();
+            let elem = self.elements.get(elem_i).unwrap();
+
+            let parent_elem = elem.parent.map_or(None, |i| self.elements.get_mut(i));
+
+            let parent_pos = if let Some(parent) = parent_elem {
+                parent.world_position
+            } else {
+                Vec2::ZERO
+            };
+
+            let elem = self.elements.get_mut(elem_i).unwrap();
+            elem.world_position = parent_pos + elem.element.position();
+
+            for child in elem.children.iter() {
+                elements.push(*child);
+            }
+
+            match &elem.element {
+                UiElement::Rect(ui_rect) => {
+                    let color = ui_rect.color;
+                    ui_draws.push(UiDraw::Frame(UiFrame {
+                        position: elem.world_position,
+                        anchor: Vec2::ZERO,
+                        size: ui_rect.size,
+                        color: Vec4::new(color.x, color.y, color.z, 1.0),
+                        texture_id: WHITE_TEXTURE_INDEX,
+                        uvs: Default::default(),
+                    }));
+                }
+                _ => {}
+            }
         }
 
-        index
+        ui_draws
     }
 }
 
@@ -76,15 +129,26 @@ mod tests {
     fn build_tree() {
         let mut ctx = UiContext::new();
 
-        ctx.new_elem(
+        let pane = ctx.new_elem(
             UiRect {
                 position: vec2(0.0, 0.0),
                 size: vec2(100.0, 50.0),
-                color: vec3(0.0, 0.0, 0.0),
+                color: vec3(1.0, 1.0, 1.0),
                 ..Default::default()
             }
             .to_elem(),
-            &[],
         );
+        ctx.root(pane);
+
+        let inner_pane = ctx.new_elem(
+            UiRect {
+                size: vec2(100.0, 50.0),
+                ..Default::default()
+            }
+            .to_elem(),
+        );
+        ctx.parent(inner_pane, pane);
+
+        ctx.build_draws();
     }
 }
