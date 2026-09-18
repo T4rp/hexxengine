567dc37b477f17a6155672da6f8beb221c714f71
T4rp <why9ten@gmail.com>
2026-09-18 16:08:21 -0400

    add colliders to transform handle

--- engine/src/entities/transform_handles.rs
+++ engine/src/entities/transform_handles.rs
@@ -1,9 +1,14 @@
 use glam::{Quat, Vec3};
+use rapier3d::{
+    geometry::{ColliderBuilder, ColliderHandle, Group, InteractionGroups, InteractionTestMode},
+    math::Pose,
+};
 use thunderdome::Index;
 
 use crate::{
     components::TransformComponent,
     game::{CONE_MESH_ID, SPHERE_MESH_ID},
+    physics::{HANDLE_INTERACTION_GROUP, context::PhysicsContext},
     renderer::render_scene::{MeshNode, RenderScene},
 };
 
@@ -32,13 +37,48 @@ pub enum TransformType {
 pub struct TransformHandles {
     pub selected: Index,
     pub transform_type: TransformType,
+    pub collision_boxes: Vec<ColliderHandle>,
 }
 
 impl TransformHandles {
-    pub fn new(selected: Index, transform_type: TransformType) -> Self {
+    pub fn new(
+        selected: Index,
+        physics: &mut PhysicsContext,
+        transform_type: TransformType,
+    ) -> Self {
+        let mut collision_boxes = Vec::new();
+
+        for _ in 0..HANDLE_AXES.len() {
+            let collider = ColliderBuilder::ball(HANDLE_SIZE / 2.0)
+                .collision_groups(HANDLE_INTERACTION_GROUP)
+                .solver_groups(HANDLE_INTERACTION_GROUP)
+                .build();
+
+            let handle = physics.collider_set.insert(collider);
+
+            collision_boxes.push(handle)
+        }
+
         Self {
             selected,
             transform_type,
+            collision_boxes,
+        }
+    }
+
+    pub fn update(&mut self, physics: &mut PhysicsContext, transform: &TransformComponent) {
+        for (i, axis) in HANDLE_AXES.iter().enumerate() {
+            let direction = transform.orientation * axis;
+
+            let collider_position = transform.position
+                + direction * ((transform.size * axis).length() / 2.0 + HANDLE_OFFSET);
+
+            let collider = physics
+                .collider_set
+                .get_mut(self.collision_boxes[i])
+                .unwrap();
+
+            collider.set_position(Pose::from_translation(collider_position));
         }
     }
 
@@ -52,14 +92,18 @@ impl TransformHandles {
         for (i, axis) in HANDLE_AXES.iter().enumerate() {
             let direction = transform.orientation * axis;
 
+            let collider_position = transform.position
+                + direction * ((transform.size * axis).length() / 2.0 + HANDLE_OFFSET);
+
+            let collider_orientation = Quat::from_rotation_arc(Vec3::Y, direction);
+
             render_scene.meshes.push(MeshNode {
-                position: transform.position
-                    + direction * ((transform.size * axis).length() / 2.0 + HANDLE_OFFSET),
-                orientation: Quat::from_rotation_arc(Vec3::Y, direction),
+                position: collider_position,
+                orientation: collider_orientation,
                 size: Vec3::splat(HANDLE_SIZE),
                 color: HANDLE_AXES_COLOR[i],
                 opacity: HANDLE_OPACITY,
-                mesh_id: CONE_MESH_ID,
+                mesh_id: mesh_id,
                 is_gizmo: true,
                 ..Default::default()
             });
