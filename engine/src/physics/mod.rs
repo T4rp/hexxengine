4a708f3d8b4e6cd5f1ec0606504f873d818754f7
T4rp <why9ten@gmail.com>
2026-09-18 15:50:05 -0400

    use interaction groups for physics

--- engine/src/physics/mod.rs
+++ engine/src/physics/mod.rs
@@ -1,1 +1,22 @@
+use rapier3d::geometry::{Group, InteractionGroups, InteractionTestMode};
+
+pub const PART_GROUP: Group = Group::GROUP_1;
+pub const CHARACTER_GROUP: Group = Group::GROUP_2;
+pub const GIZMO_GROUP: Group = Group::GROUP_9;
+
+pub const PART_INTERACTION_GROUP: InteractionGroups = InteractionGroups::new(
+    PART_GROUP,
+    PART_GROUP.union(CHARACTER_GROUP),
+    InteractionTestMode::And,
+);
+
+pub const CHARACTER_INTERACTION_GROUP: InteractionGroups = InteractionGroups::new(
+    CHARACTER_GROUP,
+    PART_GROUP.union(CHARACTER_GROUP),
+    InteractionTestMode::And,
+);
+
+pub const GIZMO_INTERACTION_GROUP: InteractionGroups =
+    InteractionGroups::new(GIZMO_GROUP, GIZMO_GROUP, InteractionTestMode::And);
+
 pub mod context;
